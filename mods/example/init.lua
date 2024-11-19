83d814813ce24aad9dadfdd691674780103b91ea
Nathan <nathan.titirangi@gmail.com>
2024-11-20 11:36:39 +1300

    improve example and fix bug

--- mods/example/init.lua
+++ mods/example/init.lua
@@ -17,13 +17,19 @@ function M.post(api)
 			"data/scripts/lua_mods/mods/example/bodies/blob.bod",
 			"example.no_brain"
 		)
+		register_creature(
+			api.acquire_id("example.small"),
+			"data/scripts/lua_mods/mods/example/bodies/small.bod",
+			"example.no_brain"
+		)
 		return unpack(r)
 	end
 
 	local old_init_biomes = init_biomes
 	function init_biomes(...)
 		local r = { old_init_biomes(...) }
-		add_creature_spawn_chance("STRT", api.acquire_id("example.blob"), 0.5, 1)
+		add_creature_spawn_chance("STRT", api.acquire_id("example.blob"), 0.2, 1)
+		add_creature_spawn_chance("STRT", api.acquire_id("example.small"), 0.2, 1)
 		return unpack(r)
 	end
 end
