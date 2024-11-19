344adce37039103c55e596975ae564cb6e362110
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-11-20 11:36:39 +1300

    improve example and fix bug

--- api.lua
+++ api.lua
@@ -1,6 +1,21 @@
 ---@type table<string, string>
 local id_map = {}
 local counter = 0
+-- stylua: ignore start
+local charset = { "A", "R", "i", "z", "B", "S", "j", "0", "C", "T", "k", "1", "D", "U", "l", "2", "E", "V", "m", "3", "F", "W", "n", "4", "G", "X", "o", "5", "H", "Y", "p", "6", "I", "Z", "q", "7", "J", "a", "r", "8", "K", "b", "s", "9", "L", "c", "t", "+", "M", "d", "u", "/", "N", "e", "v", "O", "f", "w", "P", "g", "x", "Q", "h", "y"}
+-- stylua: ignore end
+local function generate(val)
+	local s = ""
+	while val ~= 0 do
+		local low = val % 64
+		s = s .. charset[1 + low]
+		val = math.floor(val / 64)
+	end
+	while s:len() < 4 do
+		s = "=" .. s
+	end
+	return s
+end
 ---@type mod_api
 local api = {
 	acquire_id = function(key)
@@ -8,15 +23,9 @@ local api = {
 		if earlier then
 			return earlier
 		end
-		local s = ""
-		while counter ~= 0 do
-			s = s .. string.char(bit.band(counter, 0xFF))
-			counter = bit.rshift(counter, 8)
-		end
-		while s:len() < 4 do
-			s = s .. "\n" -- lua_modloader reserves the newline character for its id padding
-			-- we also use left to right generation so it probably won't conflict with the actual modloader
-		end
+		local s = generate(counter)
+		print(s, counter, "\n")
+		id_map[key] = s
 		counter = counter + 1
 		return s
 	end,
