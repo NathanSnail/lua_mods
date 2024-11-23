8a0ebb54601d188b7f8c4cc3612a2a55dc8741a6
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-11-23 21:00:26 +1300

    improve id assignment

--- api.lua
+++ api.lua
@@ -7,14 +7,14 @@ LUA_MODLOADER_CONFIG = LUA_MODLOADER_CONFIG or nil
 local id_map = {}
 local counter = 0
 -- stylua: ignore start
-local charset = { "A", "R", "i", "z", "B", "S", "j", "0", "C", "T", "k", "1", "D", "U", "l", "2", "E", "V", "m", "3", "F", "W", "n", "4", "G", "X", "o", "5", "H", "Y", "p", "6", "I", "Z", "q", "7", "J", "a", "r", "8", "K", "b", "s", "9", "L", "c", "t", "+", "M", "d", "u", "/", "N", "e", "v", "O", "f", "w", "P", "g", "x", "Q", "h", "y"}
+local charset = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","-",")","(","*","&","^","%","$","#","@","!","[","]"}
 -- stylua: ignore end
 local function generate(val)
 	local s = ""
 	while val ~= 0 do
-		local low = val % 64
+		local low = val % #charset
 		s = s .. charset[1 + low]
-		val = math.floor(val / 64)
+		val = math.floor(val / #charset)
 	end
 	while s:len() < 4 do
 		s = "=" .. s
@@ -30,7 +30,7 @@ local api = {
 			return earlier
 		end
 		local s = generate(counter)
-		print(s, counter, "\n")
+		print("id for ", key, " is ", s, " with index ", counter, "\n")
 		id_map[key] = s
 		counter = counter + 1
 		---@diagnostic disable-next-line: return-type-mismatch
