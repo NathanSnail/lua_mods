8ef98628b7212d691102f73813e1a48e08fa2083
Nathan <nathan.titirangi@gmail.com>
2024-11-25 21:23:14 +1300

    fix default modlist again :(

--- mod_list.lua
+++ mod_list.lua
@@ -2,7 +2,6 @@
 LUA_MODLOADER_MOD_LIST = {
 	-- You can use a table like this to pass settings to the mod, read the mods README.md to figure out what settings it can use
 	{ "example", { spawn_rates = 0.2 } },
-	-- { "xp_scaler", { global = 1000, biome = { strt = 50000 } } },
 	-- "really_cool_mod",
 	-- "other_cool_mod",
 }
