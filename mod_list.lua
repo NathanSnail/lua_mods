14ed814f9a2fb1b7722fa244cfe0eded256cf237
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-25 16:56:12 +1300

    fix modlist

--- mod_list.lua
+++ mod_list.lua
@@ -1,8 +1,7 @@
 ---@type mod[]
 LUA_MODLOADER_MOD_LIST = {
 	-- You can use a table like this to pass settings to the mod, read the mods README.md to figure out what settings it can use
-	-- { "example", { spawn_rates = 0.2 } },
-	"dark_depths",
+	{ "example", { spawn_rates = 0.2 } },
 	-- "really_cool_mod",
 	-- "other_cool_mod",
 }
