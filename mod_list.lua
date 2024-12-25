ddaf8104364a4fd81256eab3412b2c84a4e83586
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-25 16:55:16 +1300

    new struct fields

--- mod_list.lua
+++ mod_list.lua
@@ -1,7 +1,8 @@
 ---@type mod[]
 LUA_MODLOADER_MOD_LIST = {
 	-- You can use a table like this to pass settings to the mod, read the mods README.md to figure out what settings it can use
-	{ "example", { spawn_rates = 0.2 } },
+	-- { "example", { spawn_rates = 0.2 } },
+	"dark_depths",
 	-- "really_cool_mod",
 	-- "other_cool_mod",
 }
