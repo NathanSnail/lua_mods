c75b3025bb45644d73f90aa7b5704364e880a214
Nathan <nathan.titirangi@gmail.com>
2025-01-01 11:56:18 +1300

    fix crash for mods missing a version

--- post.lua
+++ post.lua
@@ -4,7 +4,7 @@ local old_creature_list = creature_list -- printing is only available in these l
 function creature_list(...)
 	print("\nActive mods:\n")
 	for _, v in ipairs(LUA_MODLOADER_LOADED_MODS) do
-		print(v.name .. ((" - " .. v.callbacks.version) or "") .. "\n")
+		print(v.name .. (v.callbacks.version and (" - " .. v.callbacks.version) or "") .. "\n")
 	end
 	if #LUA_MODLOADER_ERRORS ~= 0 then
 		game_print("Errors occured when loading mods")
